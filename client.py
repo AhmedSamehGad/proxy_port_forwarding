21f6530cf3539250b834ac4129e880f107b3ea6a
AhmedSamehGad <AhmedSamehGad>
2025-10-07 22:24:54 +0300

    solve_errors

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ FORWARDING_RULES = [
     {"local_port": 3306, "proxy_host": "5.tcp.eu.ngrok.io", "proxy_port": 19745},
     {"local_port": 3308, "proxy_host": "5.tcp.eu.ngrok.io", "proxy_port": 19745},
     {"local_port": 22, "proxy_host": "5.tcp.eu.ngrok.io", "proxy_port": 19745},
-    {"local_port": 80~, "proxy_host": "5.tcp.eu.ngrok.io", "proxy_port": 19745},
+    {"local_port": 80, "proxy_host": "5.tcp.eu.ngrok.io", "proxy_port": 19745},
 ]
 
 LOCAL_BIND = "0.0.0.0"
